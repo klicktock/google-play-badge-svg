dff07b3cae2e96c23ed26e49b4fd8bb42f4b35e4
Steve <stevenpatrickrichey@gmail.com>
2014-09-18 18:40:59 -0500

    Update get-localized.js

--- get-localized.js
+++ get-localized.js
@@ -4,29 +4,20 @@
  * @author Steve Richey
  * @license MIT
  */
-var GetLocalizedPlayBadge = (function()
-{
-  var PLAY_BADGE_IMG_PATH = 'http://steverichey.github.io/google-play-badge-svg/img/{{locale}}_get.svg';
-  
-  // Public method
-  function forCurrentLocale()
-  {
-    var locale = navigator.language || navigator.browserLanguage,
-    
-    return '<img src="' + _badgeFor(locale) + '" onerror="this.src=\'' + _badgeFor('en') + '\';" alt="Google Play"/>';
-  }
-  
-  // Private methods
-  function _badgeFor(locale)
-  {
-    var tinylocale = locale.toLowerCase().substring(0, 2);
-
-    // should check for dialects here!
-
-    return PLAY_BADGE_IMG_REF.replace('{{locale}}', locale);
-  }
-  
-  return {
-    forCurrentLocale
-  };
+var GetLocalizedPlayBadge = (function () {
+        'use strict';
+        var PLAY_BADGE_IMG_PATH = 'http://steverichey.github.io/google-play-badge-svg/img/{locale}_get.svg';
+        // Public method
+        function forCurrentLocale() {
+                var locale = navigator.language || navigator.browserLanguage;
+                return '<img src="' + _badgeFor(locale) + '" onerror="this.src=\'' + _badgeFor('en') + '\';" alt="Google Play"/>';
+        }
+        function _badgeFor(locale) {
+                var tinylocale = locale.toLowerCase().substring(0, 2);
+                // should check for dialects here!
+        return PLAY_BADGE_IMG_REF.replace('{locale}', locale);
+        }
+        return {
+                forCurrentLocale
+        };
 }());
