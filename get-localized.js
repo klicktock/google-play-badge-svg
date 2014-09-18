e00ec2980cf6e96dc18d81acd37b20cb996642b0
Steve <stevenpatrickrichey@gmail.com>
2014-09-18 18:50:52 -0500

    Update get-localized.js

--- get-localized.js
+++ get-localized.js
@@ -4,20 +4,17 @@
  * @author Steve Richey
  * @license MIT
  */
-var GetLocalizedPlayBadge = (function () {
+var GetLocalizedPlayBadge = GetLocalizedPlayBadge || {};
+
+GetLocalizedPlayBadge.forCurrentLocale = GetLocalizedPlayBadge.forCurrentLocale || function (desiredWidth) {
         'use strict';
+        desiredWidth = desiredWidth || {};
         var PLAY_BADGE_IMG_PATH = 'http://steverichey.github.io/google-play-badge-svg/img/{locale}_get.svg';
-        // Public method
-        function forCurrentLocale() {
-                var locale = navigator.language || navigator.browserLanguage;
-                return '<img src="' + _badgeFor(locale) + '" onerror="this.src=\'' + _badgeFor('en') + '\';" alt="Google Play"/>';
-        }
-        function _badgeFor(locale) {
+        var _badgeFor = function(locale) {
                 var tinylocale = locale.toLowerCase().substring(0, 2);
                 // should check for dialects here!
-        return PLAY_BADGE_IMG_REF.replace('{locale}', locale);
+                return PLAY_BADGE_IMG_REF.replace('{locale}', locale);
         }
-        return {
-                forCurrentLocale
-        };
-}());
+        var locale = navigator.language || navigator.browserLanguage;
+        document.write('<img src="' + _badgeFor(locale) + '" onerror="this.src=\'' + _badgeFor('en') + '\';" alt="Google Play"/>');
+};
